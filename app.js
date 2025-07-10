e7d8992b5842bea2dba5248c1c175bb5b8c0c67f
Andrès Sierra <wasierran@puce.edu.ec>
2025-07-10 18:12:42 -0500

    creacion del archivo app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// creacion del archivo principal app.js 
+import express from 'express';
+import tareasRoutes from './routes/tareasRoutes.js';
+const app = express();
+app.use(express.json());
+app.use('/tasks', tareasRoutes);
+app.listen(3000, () => {
+console.log('Servidor iniciado en http://localhost:3000');
+});
+export default app;
